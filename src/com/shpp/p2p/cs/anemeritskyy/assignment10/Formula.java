db52528d57d238f667953a0347e0acdad974c622
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2024-09-17 12:48:11 +0300

    Return previous solution

---
+++ src/com/shpp/p2p/cs/anemeritskyy/assignment10/Formula.java
@@ -0,0 +1,45 @@
+package com.shpp.p2p.cs.anemeritskyy.assignment10;
+
+import java.util.Map;
+
+/**
+ * This class is util for replacing variables to real number
+ */
+public class Formula {
+    /**
+     * Formula without variables
+     */
+    private String formula;
+
+    /**
+     * Constructor replace all variables to numbers
+     *
+     * @param formula   formula inputted by user
+     * @param variables collection of variables
+     */
+    public Formula(String formula, Map<String, Double> variables) {
+        this.formula = formula;
+        for (Map.Entry<String, Double> variable : variables.entrySet()) {
+            this.formula = this.formula.replaceAll(String.valueOf(variable.getKey()), String.valueOf(variable.getValue()));
+        }
+        if (this.formula.startsWith("--")) {
+            this.formula = this.formula.substring(2);
+        }
+    }
+
+    /**
+     * Method for returning formula
+     *
+     * @return formula without variables
+     */
+    public String getFormula() {
+        return formula;
+    }
+
+    @Override
+    public String toString() {
+        return "Formula{" +
+                "formula='" + formula + '\'' +
+                '}';
+    }
+}
---
+++ src/com/shpp/p2p/cs/anemeritskyy/assignment10/Run.java
@@ -0,0 +1,10 @@
+package com.shpp.p2p.cs.anemeritskyy.assignment10;
+
+/**
+ * Use this class to simplify check expression
+ */
+public class Run {
+    public static void main(String[] args) {
+        Assignment10Part1.main(new String[]{"a - b + c * 3 + 2^2", "a = 3", "b = 2", "c = 1"});
+    }
+}
